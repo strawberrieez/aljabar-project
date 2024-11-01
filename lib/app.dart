e1ea8f6cbb0a2299f20fed590b59282898e87edc
sekarr <sekarayufatmasari722@gmail.com>
2024-11-02 01:08:20 +0700

    feat: landing page(?)

---
+++ lib/app.dart
@@ -0,0 +1,14 @@
+import 'package:aljabar_project/landingpage.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LandingPage(),
+    );
+  }
+}
